3d14bfe813d91bf4af8f9047c3744c2ba385fad9
Lucaslpena <lucaslpena@gmail.com>
2020-09-18 13:04:08 +0200

    removed events

--- pages/events.js
+++ pages/events.js
@@ -1,15 +1,15 @@
-import {Footer, Image, Menu} from "../components";
+// import {Footer, Image, Menu} from "../components";
+// //
+// const Events = () => {
+//   return(
+//     <main className={'events'}>
+//       <section>
+//         <h1>dis events</h1>
+//         <Image src={'team.png'} />
+//       </section>
+//       <Footer/>
+//     </main>
+//   )
+// }
 //
-const Events = () => {
-  return(
-    <main className={'events'}>
-      <section>
-        <h1>dis events</h1>
-        <Image src={'team.png'} />
-      </section>
-      <Footer/>
-    </main>
-  )
-}
-
-export default Events
+// export default Events
