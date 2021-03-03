c32e64008626345cc884b12fdcaa84e800f98059
Lucaslpena <lucaslpena@gmail.com>
2021-03-03 20:07:50 +0100

    dynamic pages for >9.3 nextjs on [lab]

--- pages/lab/[lab].js
+++ pages/lab/[lab].js
@@ -5,8 +5,8 @@ import {LabData} from "../../constants";
 import {Footer} from "../../components";
 import { useWindowSize } from "react-use";
 
-const Lab = ({slug}) => {
-  const lab = LabData.find(l => l.encoded === ((slug) ? slug[0] : null));
+const Lab = ({ lab }) => {
+  // const lab = LabData.find(l => l.encoded === ((slug) ? slug[0] : null));
   const {width, height} = useWindowSize();
 
   useEffect(() => {
@@ -43,10 +43,30 @@ const Lab = ({slug}) => {
   )
 }
 
-Lab.getInitialProps = async ({ query }) => {
-  const { slug } = query;
-  return { slug };
-};
+// Lab.getInitialProps = async ({ query }) => {
+//   const { slug } = query;
+//   return { slug };
+// };
+
+export async function getStaticProps(context) {
+  // const { lab } = context.params;
+  console.log(context)
+
+  // const lab = LabData.find(l => l.encoded === ((slug) ? slug[0] : null));
+  console.log(context.params.lab)
+  // return { props: { lab:  LabData.find(l => l.encoded === ((slug) ? slug[0] : null))} };
+  return {
+    props: {
+      lab:  LabData.find(l => l.encoded === context.params.lab)
+    },
+  }
+}
+export async function getStaticPaths() {
+  const paths = LabData.map(l => ({
+    params: { lab: l.encoded },
+  }))
+  return { paths, fallback: false }
+}
 
 
 export default Lab
