2376bca7c930299a2adca2ca94a53a9f0641dfa9
Lucaslpena <lucaslpena@gmail.com>
2021-03-09 12:22:43 +0100

    top bannere almost ready for slot machine

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Card, Landing, removeWebpFormat } from "../components";
+import {Card, Landing, removeWebpFormat, Icon, ICONS, THEME, DividedContent} from "../components";
 import {useRouter} from "next/router";
 import { AnimatePresence, motion } from "framer-motion";
 import { FeatureContent } from "../constants";
@@ -35,6 +35,38 @@ const Home = () => {
     setSlide((slide+2) % FeatureContent.length );
   }
 
+  const bannerAnimation = {
+    initial:{
+      opacity: 0
+    },
+    animate:{
+      opacity: 1,
+      transition: {
+        duration: .25,
+        delay: .5,
+        staggerChildren: .2,
+        ease: 'easeInOut',
+      }
+    },
+    exit:{
+      opacity: 0
+    }
+  }
+
+  const bannerVariant = {
+    initial: {
+      opacity: 0,
+      y: 100,
+    },
+    animate: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        duration: .12,
+      }
+    }
+  }
+
   const generateSlide = (data, key) => {
     return(
       <Card key={key} content={data} nextSlide={nextSlide} prevSlide={prevSlide} />
@@ -44,42 +76,89 @@ const Home = () => {
   return(
     <motion.main
       className={'Index'}
-      style={background}
-      initial={{
-        backgroundSize: "400% 400%",
-      }}
-      animate={{
-        backgroundSize: '100% 100%',
-        transition: {
-          duration: 12,
-          easings: "backInOut",
-          repeat: Infinity,
-          repeatType: "mirror"
-        }
-      }}
-      exit={{
-        opacity: 0,
-        transition:{
-          duration: .25,
-          easings: "linear"
-        }
-      }}
     >
-      <AnimatePresence exitBeforeEnter>
-        {/*{ (slide === 3) &&*/}
-        {/*<Landing exploreClick={() => { setSlide(0)} } />*/}
+      <section className={'pre-banner'}>
+        <div className={'section-content'}>
 
-        {/*}*/}
-        { (slide === 0) &&
-        generateSlide( FeatureContent[0], 0 ) }
-        }
-        { (slide === 1) &&
-        generateSlide( FeatureContent[1], 1 ) }
-        }
-        { (slide === 2) &&
-        generateSlide( FeatureContent[2], 2 ) }
-        }
-      </AnimatePresence>
+          <motion.div
+            className={'bad-roller'}
+            initial={"initial"}
+            animate={"animate"}
+            exit={"exit"}
+            variants={bannerAnimation}
+          >
+            <motion.div><motion.h2 variants={bannerVariant}>
+                Our
+              </motion.h2>
+            </motion.div>
+            <motion.div><motion.h2 variants={bannerVariant}>
+                Financial
+              </motion.h2>
+            </motion.div>
+            <motion.div><motion.h2 variants={bannerVariant}>
+                Futures
+              </motion.h2>
+            </motion.div>
+            <motion.div><motion.h2 variants={bannerVariant}>
+                are
+              </motion.h2>
+            </motion.div>
+            <motion.div><motion.h2 variants={bannerVariant}>
+                Fancy
+              </motion.h2>
+            </motion.div></motion.div>
+
+          <div>
+            <p>
+              Futurity Studio is a research and design studio at the intersection of design, technology, social, and business innovation helping communities, and corporates build better futures faster.
+            </p>
+            <em>learn more
+              {/*<Icon icon={ICONS.ARROW_RIGHT} theme={THEME.LIGHT}/>*/}
+            </em>
+          </div>
+
+
+
+        </div>
+      </section>
+      <motion.div
+        style={background}
+        initial={{
+          backgroundSize: "400% 400%",
+        }}
+        animate={{
+          backgroundSize: '100% 100%',
+          transition: {
+            duration: 12,
+            easings: "backInOut",
+            repeat: Infinity,
+            repeatType: "mirror"
+          }
+        }}
+        exit={{
+          opacity: 0,
+          transition:{
+            duration: .25,
+            easings: "linear"
+          }
+        }}
+      >
+        <AnimatePresence exitBeforeEnter>
+          {/*{ (slide === 3) &&*/}
+          {/*<Landing exploreClick={() => { setSlide(0)} } />*/}
+
+          {/*}*/}
+          { (slide === 0) &&
+          generateSlide( FeatureContent[0], 0 ) }
+          }
+          { (slide === 1) &&
+          generateSlide( FeatureContent[1], 1 ) }
+          }
+          { (slide === 2) &&
+          generateSlide( FeatureContent[2], 2 ) }
+          }
+        </AnimatePresence>
+      </motion.div>
     </motion.main>)
 }
 
